a335675cd7a5a397a2a9edb28a57a151a5972490
Tanuj <tanujx03@outlook.com>
2026-06-05 20:00:42 +0530

    Learn f-strings and user input handling

---
+++ 04_inputOutput.py
@@ -0,0 +1,12 @@
+type_of_string = "formatted"
+print(f"This is a {type_of_string} string")
+
+# input function for input from user
+
+name = input("What is your name? ")
+print(f"Hello {name}!")
+print(type(name)) # <class 'str'>
+
+age = int(input("Whats your age? "))
+print(f"You are {age} years old.")
+print(type(age)) # <class 'int'>
