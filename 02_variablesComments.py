0291ceb2a2bc06ece6e7777d096a7308c10d332c
Tanuj <tanujx03@outlook.com>
2026-06-05 06:47:11 +0530

    Learn Python variables and data types

---
+++ 02_variablesComments.py
@@ -0,0 +1,31 @@
+# Comments
+
+"""
+Hello
+this
+is
+multiline, not a comment, but a docstring.
+"""
+
+# Variables
+
+english = "Hello Duniya!"
+age = 122
+decimal = 3.14
+fraction = 22/7
+complex_number = 1 + 2j
+is_python_fun = True
+
+print(english)
+print(age)
+print(decimal)
+print(fraction)
+print(complex_number)
+print(is_python_fun)
+
+print(type(english))
+print(type(age))
+print(type(decimal))
+print(type(fraction))
+print(type(complex_number))
+print(type(is_python_fun))
