5f4827c8ffdaf66a46be2dba1c4739fe6f4fdaef
Tanuj <tanujx03@outlook.com>
2026-06-06 20:43:50 +0530

    Learn Python conditional statements

---
+++ 06_conditionalStatement.py
@@ -0,0 +1,24 @@
+# Conditional statements are used to perform different actions based on different conditions.
+# The most common conditional statements in Python are if, elif, and else.
+
+# Example of if statement
+x = 10
+if x > 5:
+    print("x is greater than 5")
+
+# Example of if-else statement
+y = 3
+if y > 5:
+    print("y is greater than 5")
+else:
+    print("y is not greater than 5")
+
+# Example of if-elif-else statement
+z = 7
+if z > 10:
+    print("z is greater than 10")
+elif z > 5:
+    print("z is greater than 5 but not greater than 10")
+else:
+    print("z is not greater than 5")
+
